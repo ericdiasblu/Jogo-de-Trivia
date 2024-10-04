2b4313083d6c5d3f14f4c31700f029a6d2664c4a
ericdiasblu <ericdiasblu@gmail.com>
2024-10-04 10:23:59 -0300

    organization and animations

---
+++ menu/script.js
@@ -0,0 +1,12 @@
+document.addEventListener('keydown', function(event) {
+    if (event.code === 'Space') {
+        const menu = document.querySelector('.fade-in'); // Alvo do fade-in
+        menu.classList.remove('fade-in'); // Remove a classe fade-in, se necessário
+        menu.classList.add('zoom-out'); // Adiciona a animação de zoom-out
+
+        // Após a animação, redireciona para a página do jogo
+        setTimeout(() => {
+            window.location.href = '../geral-theme/game.html'; // Caminho para o jogo
+        }, 500); // Tempo da animação
+    }
+});
